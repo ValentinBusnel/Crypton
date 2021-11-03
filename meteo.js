f46839b7eba7669e95688dadb15975688d061bdc
Valentin Busnel <valentinbusnel27@gmail.com>
2021-11-03 17:05:29 +0100

    classement add

--- meteo.js
+++ meteo.js
@@ -14,31 +14,23 @@ fetch('https://api-football-standings.azharimm.site/leagues')
     }
   })
 
-
-var myHeaders = new Headers();
+const myHeaders = new Headers();
 myHeaders.append("x-rapidapi-key", "76e77c5ebc99ab3db8216510b4a095b1");
 myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
 
-var requestOptions = {
+const requestOptions = {
   method: 'GET',
   headers: myHeaders,
   redirect: 'follow'
 };
 
-fetch("https://v3.football.api-sports.io/standings?league=39&season=2021", requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result.response[0].league.standings[0][0].team.name))
-  .catch(error => console.log('error', error));
+fetch(`https://v3.football.api-sports.io/standings?league=37&season=2021`, requestOptions)
+  .then (response => response.json())
+  .then((data) => {
+    console.log(data);
+  });
 
-
-const tBody = document.querySelector("tbody")
-const tr = tBody.querySelector("tr")
-const td = tr.querySelectorAll("td")
-let league = 39;
-
-
-
-const displayClassementPl = () => {
+const displayClassementPl = (league) => {
   fetch(`https://v3.football.api-sports.io/standings?league=${league}&season=2021`, requestOptions)
     .then(res => {
       if (res.ok) {
@@ -70,6 +62,37 @@ const displayClassementPl = () => {
 
 
 const pL = document.querySelector(".premiere-league")
+const ligue1 = document.querySelector(".ligue-1")
+const bundesliga = document.querySelector(".bundesliga")
+const serieA = document.querySelector(".serie-A")
+const laLiga = document.querySelector(".la-Liga")
+
 pL.addEventListener("click", (event) => {
-  displayClassementPl();
+  document.querySelector("tbody").innerHTML = "";
+  document.querySelector("thead").classList.remove("d-none");
+  displayClassementPl(39); //39 PL |
+});
+
+ligue1.addEventListener("click", (event) => {
+  document.querySelector("tbody").innerHTML = "";
+  document.querySelector("thead").classList.remove("d-none");
+  displayClassementPl(61); //39 PL |
+});
+
+bundesliga.addEventListener("click", (event) => {
+  document.querySelector("tbody").innerHTML = "";
+  document.querySelector("thead").classList.remove("d-none");
+  displayClassementPl(78); //39 PL |
+});
+
+serieA.addEventListener("click", (event) => {
+  document.querySelector("tbody").innerHTML = "";
+  document.querySelector("thead").classList.remove("d-none");
+  displayClassementPl(135); //39 PL |
+});
+
+laLiga.addEventListener("click", (event) => {
+  document.querySelector("tbody").innerHTML = "";
+  document.querySelector("thead").classList.remove("d-none");
+  displayClassementPl(140); //39 PL |
 });
